9040dfdc7fa8f9dd4243406ff24d6fef8a502fe6
sivakrishna2816 <sivakrishnaeee209@gmail.com>
2024-11-22 12:43:37 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,8 +3,6 @@ public class Test
 	public static void main(String[] args) 
 	{
 		System.out.println("welcome to sathish siva!");
-        System.out.println("welcome to lakshmi!");
-	}
-}
+		system.out.println("welcome to lakshmi!");
 	}
 }
