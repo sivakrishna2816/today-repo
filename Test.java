8801fd4769a1cc6eb6ec138ef92d36736d9af4ef
sivakrishna2816 <sivakrishnaeee209@gmail.com>
2024-11-22 12:39:03 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,10 @@
-public class Test{
-    public static void main(string args[]){
-       system.out.println("welcome to devops");
-     }
-   }
+public class Test
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("welcome to sathish siva!");
+        System.out.println("welcome to lakshmi!");
+	}
+}
+	}
+}
